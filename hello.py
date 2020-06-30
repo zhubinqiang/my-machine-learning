f736856c86a5d9c93b17407f2389c3ecf7f9de68
ZhuBinQiang <zhu.binqiang@163.com>
2020-06-30 23:48:30 +0800

    init

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# **********************************************
+# @file: hello.py
+# @author: ZhuBinQiang <zhu.binqiang@163.com>
+# @create time: 2020-06-11 23:10:48
+# @last modified: 2020-06-11 23:10:48
+# @description:
+# **********************************************
+
+
+def hello(msg):
+        print("Hello %s !" %(msg))
+
+hello("今晚打老虎")
+
+
